5b9cc1bd4cd287fd8fcbdd64f029aee8876b66bf
DaniBrandon <dbrandon2994@gmail.com>
2022-02-16 17:33:46 +0000

    Add files via upload

---
+++ Reservas/Reservas/urls.py
@@ -0,0 +1,28 @@
+"""Reservas URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django import views
+from django.contrib import admin
+from django.urls import path
+from ReservasApp.views import habitacion
+from ReservasApp.views import selecthabitacion
+from ReservasApp.views import HabitacionListView
+
+urlpatterns = [
+    path('listahabitacion/', HabitacionListView.as_view(), name='habitacion_list.html'),
+    path('habitacion/', habitacion),
+    path('selecthabitacion/', selecthabitacion),
+    path('admin/', admin.site.urls),
+]
